56ed28cb9c6592f7ec2049f9ff5ac6eb18ea24c1
Nikolay Sivko <n.sivko@gmail.com>
2014-11-11 22:41:38 +0300

    * exit on stdin close

--- src/main/java/org/vafer/jmx/Main.java
+++ src/main/java/org/vafer/jmx/Main.java
@@ -3,6 +3,10 @@ package org.vafer.jmx;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public final class Main {
 
     @Parameter(names = "-config", description = "path to config file", required = true)
@@ -15,9 +19,13 @@ public final class Main {
         if (agent) {
             new Agent(configPath).start();
             // just running the agent until the jvm is terminated
-            while(true) {
-                Thread.sleep(5*1000);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String s;
+            while ((s = br.readLine()) != null && s.length() != 0) {
+                System.out.println(s);
             }
+            System.exit(0);
         } else {
             Exporter exporter = new Exporter();
             Exporter.Config config = exporter.load(configPath);
@@ -34,7 +42,6 @@ public final class Main {
             cli.usage();
             System.exit(1);
         }
-
         m.run();
     }
 }
